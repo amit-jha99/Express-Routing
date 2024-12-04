f269bc04f34860040ef77e653496759f967b38ec
amit-jha99 <amitjha121299@gmail.com>
2024-12-04 14:46:21 +0530

    HTTP request methods

--- src/app.js
+++ src/app.js
@@ -4,7 +4,26 @@ const app = express()
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', (req, res) => {
   res.send('hello world')
-})
+});
+
+
+// respond with "hello world" when a POST request is made to the homepage
+app.post('/user', (req, res) => {
+    res.send('user created!!')
+  })
+
+  // respond with "hello world" when a PUT request is made to the homepage
+app.put('/user', (req, res) => {
+    res.send('user updated!!')
+  })
+  
+
+
+   // respond with "hello world" when a DELETE request is made to the homepage
+app.delete('/user', (req, res) => {
+    res.send('user deleted!!')
+  })
+  
 
 app.listen(3000,()=>{
    console.log("Server is listening at port 3000")
